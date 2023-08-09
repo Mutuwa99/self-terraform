61404c3527a82fac809c668b0a0c7af91900dde4
Noble Mulaudzi <isayamulaudzi@gmail.com>
2023-08-09 14:46:54 +0200

    my first commit

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+
+
+# Hi  @4Sure team , note that i have removed my AWS CRED here, due to the security issues that may arise if i push the code to github with these keys. Thanks
